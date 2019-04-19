8f5daf39ea20d21d3cff4df87c9ad15ff66df191
Andrei Tenescu <teny96@gmail.com>
2019-04-19 23:34:54 +0300

    Cleaned up simple agent; began work on architecture drawing

--- simple_agent.py
+++ simple_agent.py
@@ -38,7 +38,7 @@ def main(unused_argv):
         while True:
             with sc2_env.SC2Env(
                     map_name="Simple64",
-                    players=[sc2_env.Agent(sc2_env.Race.terran),
+                    players=[sc2_env.Agent(sc2_env.Race.protoss),
                              sc2_env.Bot(sc2_env.Race.random,
                                          sc2_env.Difficulty.very_easy)],
                     agent_interface_format=features.AgentInterfaceFormat(
